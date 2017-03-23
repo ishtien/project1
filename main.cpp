0e18880ac25dd1d7ba9a7820e1fd968c6e375037
ishtien <203isthebest@gmail.com>
2017-03-24 00:48:33 +0800

    project1

--- main.cpp
+++ main.cpp
@@ -21,23 +21,24 @@ int main(){
 				cin>>map1[j][k];
 			}
 		}
-		int x=rand()%10+10,y=rand()%10+10;
+		int x=rand()%10+5,y=rand()%10+20;
 		if(mana>0){
 			int tmp=rand();
 			tmp=tmp%2;
+//			cout<<"1 5 12 20 ";
 //			cout<<"1 "<<card1<<"3"<<"9";
 //			cout<<"0";
 			if(tmp==0){
 				cout<<"1 1 "<<x<<" "<<y<<" ";
-				cout<<"1 2 "<<x<<" "<<y<<" ";
-				cout<<"1 3 "<<x<<" "<<y<<" ";
-				cout<<"1 4 "<<x<<" "<<y<<" ";
+				cout<<"1 2 "<<y<<" "<<x<<" ";
+				cout<<"1 3 "<<x<<" "<<x<<" ";
+				cout<<"1 4 "<<y<<" "<<y<<" ";
 				cout<<"0 "<<endl;
 			}else if(tmp==1){
 				cout<<"1 5 "<<x<<" "<<y<<" ";
-				cout<<"1 6 "<<x<<" "<<y<<" ";
-				cout<<"1 7 "<<x<<" "<<y<<" ";
-				cout<<"1 8 "<<x<<" "<<y<<" ";
+				cout<<"1 6 "<<y<<" "<<x<<" ";
+				cout<<"1 7 "<<x<<" "<<x<<" ";
+				cout<<"1 8 "<<y<<" "<<y<<" ";
 				cout<<"0 "<<endl;
 			}
 /*			}else if(tmp==2){
@@ -50,17 +51,6 @@ int main(){
 				cout<<"0 "<<endl;
 			}else if(tmp==4){
 				cout<<"1 5"<<x<<" "<<y<<" ";
-				cout<<"0 "<<endl;
-			}else if(tmp==5){
-				cout<<"1 6"<<x<<" "<<y<<" ";
-				cout<<"0 "<<endl;
-			}else if(tmp==6){
-				cout<<"1 7"<<x<<" "<<y<<" ";
-				cout<<"0 "<<endl;
-			}else if(tmp==7){
-				cout<<"1 8"<<x<<" "<<y<<" ";
-				cout<<"0 "<<endl;
-			}
 */		}
 	}
 	return 0;
