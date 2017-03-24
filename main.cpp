4c7d97c4e9c60902ac1fd3e00fc77694870ed909
ishtien <203isthebest@gmail.com>
2017-03-24 12:20:54 +0800

    where is my code?

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
 	char map1[22][52];
 	int countdown,mana,card1,card2,card3,card4;
 	srand((unsigned)time(NULL));
-	cout<<"5 2 3 4 1 6 8 7"<<endl;
+	cout<<"4 2 8 1 3 6 5 7"<<endl;
 	while(1){
 		cin >> countdown >> mana >> card1 >> card2 >> card3 >> card4;
 		for(int j=0;j<22;j++){
@@ -29,16 +29,16 @@ int main(){
 //			cout<<"1 "<<card1<<"3"<<"9";
 //			cout<<"0";
 			if(tmp==0){
-				cout<<"1 1 "<<x<<" "<<y<<" ";
-				cout<<"1 2 "<<y<<" "<<x<<" ";
-				cout<<"1 3 "<<x<<" "<<x<<" ";
-				cout<<"1 4 "<<y<<" "<<y<<" ";
+				cout<<"1 "<<"2 7 "<<"19 ";
+				cout<<"1 "<<"1 18 "<<"17 ";
+				cout<<"1 "<<"8 5 "<<"23 ";
+				cout<<"1 "<<"4 17 "<<"20 ";
 				cout<<"0 "<<endl;
 			}else if(tmp==1){
-				cout<<"1 5 "<<x<<" "<<y<<" ";
-				cout<<"1 6 "<<y<<" "<<x<<" ";
-				cout<<"1 7 "<<x<<" "<<x<<" ";
-				cout<<"1 8 "<<y<<" "<<y<<" ";
+				cout<<"1 "<<"5 6 "<<"19 ";
+				cout<<"1 "<<"6 4 "<<"17 ";
+				cout<<"1 "<<"3 6 "<<"23 ";
+				cout<<"1 "<<"7 16 "<<"23 ";
 				cout<<"0 "<<endl;
 			}
 /*			}else if(tmp==2){
